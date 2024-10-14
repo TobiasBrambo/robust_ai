5d162627c3bf11971a7c530b8c63b626e88469f9
TobiasMB <tobias.m.brambo@gmail.com>
2024-10-14 20:18:14 +0200

    start resnet

--- README.md
+++ README.md
@@ -2,7 +2,22 @@
 
 
 ### TODO:
+- [ ] TRACK TRAINING TIME ETC. FOR ALL EXPERIMENTS. FOR PLOTS
 - [ ] Find best CNN setup to best match the different experiments
 - [ ] Setup nice experiment pipeline (?)
 - [ ] Implement different training schemes
 - [ ] Implement different experiments, to test different training schemes
+
+
+
+potential setup:
+
+final table:
+- train each model using each method
+- test on a set of attacks, regardless of which have been used during training.
+    - FGSM
+    - ...
+- test all on attack method for any of the models
+    - ...
+
+- for ensamble methods, could get the accuracies for multiple different combinations of models
--- robust_ai/adversarial_retraining.py
+++ robust_ai/adversarial_retraining.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
+import torchvision.models as models
 import torchvision.transforms as transforms
 
 import foolbox as fb
@@ -31,10 +32,7 @@ class SimpleCNN(nn.Module):
 
 
 
-def train_model(model, criterion, optimizer, epochs:int = 10):
-
-    fmodel = fb.PyTorchModel(model, bounds=(0, 1))
-    attack = fb.attacks.FGSM()
+def train_model(model, criterion, optimizer, fmodel, attack, trainloader, epochs:int = 10):
 
     for epoch in range(epochs):  # Iterate through epochs
         running_loss = 0.0
@@ -50,8 +48,8 @@ def train_model(model, criterion, optimizer, epochs:int = 10):
             outputs = model(inputs)
             loss = criterion(outputs, labels)
             
-            # Generate adversarial examples using Foolbox
-            adversarial_inputs = attack(fmodel, inputs, labels)
+
+            _, adversarial_inputs, _= attack(fmodel, inputs, labels, epsilons=0.01)
             
             # Compute loss on adversarial inputs
             adv_outputs = model(adversarial_inputs)
@@ -70,7 +68,97 @@ def train_model(model, criterion, optimizer, epochs:int = 10):
                 print(f'[Epoch {epoch + 1}, Batch {i + 1}] loss: {running_loss / 100:.3f}')
                 running_loss = 0.0
 
-        print('Finished Training')
+    print('Finished Training')
+
+def evaluate(model, testloader):
+
+    # correct = 0
+    # total = 0
+    #
+    # with torch.no_grad():
+    #     for data in testloader:
+    #         images, labels = data
+    #         images, labels = images.to(device), labels.to(device)
+    #
+    #         outputs = model(images)
+    #         _, pred = torch.max(outputs.data, 1)
+    #
+    #         total += labels.size(0)
+    #         correct += (pred == labels).sum().item()
+
+    clean_acc = fb.accuracy(fmodel, images, labels)
+    
+
+
+def retrain_resnet50():
+
+    batch_size = 64
+    learning_rate = 0.001
+
+    model = models.resnet50(pretrained=True).to(device)
+
+    preprocessing = dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], axis=-3)
+
+    fmodel = fb.PyTorchModel(model, bounds=(0,1), preprocessing=preprocessing)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate) 
+
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomRotation(degrees=45),
+        transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    train_dataset = torchvision.datasets.ImageNet( root="./data/raw_files", transform=transform)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+
+    for epoch in range(num_epochs):
+        total_loss = 0
+        for inputs, labels in train_loader:
+            # Move input and label tensors to the device
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # Zero out the optimizer
+            optimizer.zero_grad()
+
+            # Forward pass
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+
+            _, adversarial_inputs, _= attack(fmodel, inputs, labels, epsilons=0.01)
+            
+            # Compute loss on adversarial inputs
+            adv_outputs = model(adversarial_inputs)
+            adv_loss = criterion(adv_outputs, labels)
+            
+            # Combine the loss (you can balance clean and adversarial loss)
+            total_loss = loss + adv_loss
+            
+            # Backward pass and optimize
+            total_loss.backward()
+            optimizer.step()
+
+            # Backward pass
+            loss.backward()
+            optimizer.step()
+
+    # Print the loss for every epoch
+        print(f'Epoch {epoch+1}/{num_epochs}, Loss: {total_loss.item():.4f}')
+
+    print(f'Finished Training, Loss: {loss.item():.4f}')
+
+
+    torch.save(model.state_dict(), './trained_models')
 
 
 
@@ -78,13 +166,29 @@ def train_model(model, criterion, optimizer, epochs:int = 10):
 if __name__ == "__main__":
 
     # Load dataset (CIFAR-10 example)
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+    # transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # trainset = torchvision.datasets.CIFAR10(root='./data/raw_files', train=True, download=True, transform=transform)
+    # trainloader = torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=True)
+    #
+    # testset = torchvision.datasets.CIFAR10(root='./data/raw_files', train=False, download=True, transform=transform)
+    # print(testset.shape())
+    # testloader = torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)
+    #
+    #
+    # assert False
 
     # Initialize model, loss, and optimizer
-    model = SimpleCNN()
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    # model = SimpleCNN()
+    # criterion = nn.CrossEntropyLoss()
+    # optimizer = optim.Adam(model.parameters(), lr=0.001)
+    #
+    #
+    # fmodel = fb.PyTorchModel(model, bounds=(-1, 1))
+    # attack = fb.attacks.LinfFastGradientAttack()
+    #
+    #
+    # train_model(model, criterion, optimizer, fmodel, attack, trainloader)
+    #
+    # torch.save(model.state_dict(), 'trained_models/')
 
-    train_model(model, criterion, optimizer)
+    retrain_resnet50()
